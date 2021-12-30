0c73bbd514b33340704a366220682be12410763b
noumedem <noumedemtamiflorian@gmail.com>
2021-12-30 19:05:17 +0100

    Creation du model User

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,7 @@
-from django.db import models
-
+from django.db.models import *
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+    is_verified = BooleanField(default=False)
